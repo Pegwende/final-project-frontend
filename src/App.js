74817d1cdd799b95c153456456e82e0b26a3f8ef
Pegwende Zabsore <pegwendezabsore@Pegwendes-MacBook-Air.local>
2021-08-26 15:21:59 -0500

    react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <>
+            <h1> Welcome! </h1>
+            <h2> What do you want to eat Today? </h2>
+        </>
+    )
+}
+
+export default App;
