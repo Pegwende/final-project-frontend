35d72f890e1fdb06b839efc108f807f2c1482912
Pegwende Zabsore <pegwendezabsore@Pegwendes-MacBook-Air.local>
2021-09-01 04:00:58 -0500

    Bulma done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import Add from './components/Add'
 import Edit from './components/Edit'
+import './App.css'
 
 
 const App = () => {
@@ -52,26 +53,86 @@ const App = () => {
 
     return (
         <>
-            <Add handleCreate={handleCreate}/>
-            <h1> Welcome! </h1>
-            <h2> What do you want to eat Today? </h2>
-            <div className='foods'>
-            {
-                foods.map((food)=>{
-                    return(
-                        <div className='food'>
-                            <h3>Name: {food.name}</h3>
-                            <Edit handleUpdate={handleUpdate} theFood={food} />
-                            <img src={food.image} alt=''/>
-                            <p>Description: {food.description}</p>
-                            <p>Price: {food.price}</p>
-                            <button type='button' value={food.id} onClick={handleDelete}> Delete </button>
-                        </div>
+            <section className="hero is-primary is-medium mb-4" >
 
-                    )
-                })
-            }
-            </div>
+                <div class='hero-head'>
+                    <p className="title is-1"> Foods Website</p>
+                    <p className="subtitle is-5">List your best foods to attract customers.</p>
+                </div>
+
+                <div class='hero-body'>
+                    <div className="container has-text-centered">
+                        <h1 className="title is-1">Welcome</h1>
+                        <p class='subtitle'> Best foods in town</p>
+                    </div>
+                </div>
+                <div className="hero-footer has-text-centered">
+                    <form className="mb-5">
+                    <label className="title is-5" for='categories'>Cuisine: </label>
+                    <select name="categories" id='categories'>
+                        <option value='american'>AMERICAN</option>
+                        <option value='africans'>AFRICA</option>
+                        <option value='chinese'>CHINESE</option>
+                        <option value='italian'>ITALIAN</option>
+                        <option value='japanese'>JAPANESE</option>
+                        <option value='mexican'>MEXICAN</option>
+                        <option value='colombian'>COLOMBIAN</option>
+                        <option value='india'>INDIA</option>
+                        <input type='submit' value='submit'/>
+                    </select>
+                    </form>
+                </div>
+
+            </section>
+
+            <Add handleCreate={handleCreate}/>
+
+            <div className="block columns is-flex-direction-row is-justify-content-space-around is-flex-wrap-wrap is-narrow has-background-light">
+                {
+                    foods.map((food)=>{
+                        return(
+                            <div className=' column is-3 card has-text-centered m-5 is-justify-content-space-around is-narrow' key={food.id}>
+                                <div className='card-image is-narrow'>
+                                    <h3 className="title is-1 has-text-centered"> {food.name}</h3>
+                                    <figure className='image is-2by1'>
+                                    <img src={food.image} alt=''/>
+                                    </figure>
+                                </div>
+                                <div className='column card-content is-narrow'>
+                                    <div className='content'>
+                                        <p className="title is-5 has-text-primary-dark">Description: {food.description}</p>
+                                        <p className="title is-4 mb-1">${food.price}</p>
+                                          <Edit className="" handleUpdate={handleUpdate} theFood={food} />
+                                          <button className="button is-danger mt-1" type='button' value={food.id} onClick={handleDelete}> Delete </button>
+                                    </div>
+                                </div>
+                                </div>
+
+                        )
+                    })
+                }
+                </div>
+
+                <section className="hero is-primary is-large p-6" >
+                    <div className="hero-footer has-text-centered">
+                        <form className="mb-5">
+                        <label className="title is-5" for='categories'>Cuisine: </label>
+                        <select name="categories" id='categories'>
+                            <option value='american'>AMERICAN</option>
+                            <option value='africans'>AFRICA</option>
+                            <option value='chinese'>CHINESE</option>
+                            <option value='italian'>ITALIAN</option>
+                            <option value='japanese'>JAPANESE</option>
+                            <option value='mexican'>MEXICAN</option>
+                            <option value='colombian'>COLOMBIAN</option>
+                            <option value='india'>INDIA</option>
+                            <input type='submit' value='submit'/>
+                        </select>
+                        </form>
+                    </div>
+
+                </section>
+
 
         </>
     )
